8f4683b609629fad27e7f0a190d2aab25dca87e9
Rohan-1-3 <rohanshrestha130.com>
2025-10-08 15:17:26 +0545

    fix: configuration changes for nginx and vps support

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -235,6 +235,11 @@ const UserList = () => {
         >
           Add New User
         </Link>
+	<button onClick={async()=>{
+          await userService.seedDatabase();
+        }} className="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md transition-colors dark:text-white">
+          Populate Users
+        </button>
       </div>
 
       {/* Filters */}
